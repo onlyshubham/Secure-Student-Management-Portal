02f1b365ed64cdc240c95e9b9c6fd2a8efff2259
onlyshubham <123366290+onlyshubham@users.noreply.github.com>
2026-08-24 22:25:35 +0530

    Set up Express backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const healthRoutes = require('./routes/healthRoutes')
+
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+
+app.use('/api', healthRoutes)
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Secure Student Portal API is running',
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
