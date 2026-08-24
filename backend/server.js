db352d62bd4100cb0d9ccffea3bd95b8aacf0eb5
onlyshubham <123366290+onlyshubham@users.noreply.github.com>
2026-08-25 01:15:07 +0530

    Add environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const healthRoutes = require('./routes/healthRoutes')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
