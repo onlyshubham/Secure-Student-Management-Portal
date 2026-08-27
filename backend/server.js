7a5fa2579af7b4bb8db53ef59c634d54b53396b7
onlyshubham <123366290+onlyshubham@users.noreply.github.com>
2026-08-27 10:25:04 +0530

    checkpoint: MongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 
 const express = require('express')
 const healthRoutes = require('./routes/healthRoutes')
+const connectDB = require('./config/db')
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+connectDB()
 
 app.use(express.json())
 
